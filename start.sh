309aa53e892a8d25972b550aa7f8c0b0e8714235
Jackson Darrigo <31744342+jacksondarrigo@users.noreply.github.com>
2020-05-16 21:18:12 -0400

    Added auto-update script and moved Discord token to command-line arg

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ pip install --upgrade discord.py
 pip install --upgrade youtube-dl
 apt-get update && apt-get install -y ffmpeg
 
-python /houseband/bot_main.py $1
+python /houseband/bot_main.py $DISCORD_TOKEN
