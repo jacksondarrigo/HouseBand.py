af364128c8fdda21df5852a6fa3a5dae9298e2b1
Jackson Darrigo <31744342+jacksondarrigo@users.noreply.github.com>
2020-11-14 19:50:34 -0500

    Adjusting async coroutines for SIGTERM support

--- bot_main.py
+++ bot_main.py
@@ -42,12 +42,12 @@ if __name__ == '__main__':
         except Exception as error:
             print('{} cannot be loaded. ({})'.format(cog, error))
 
-    async def sigterm_handler(signal, frame):
+    async def sigterm_handler():
         await bot.logout()
         sys.exit(0)
 
     loop = asyncio.get_event_loop()
-    loop.add_signal_handler(signal.SIGTERM, lambda: asyncio.async(sigterm_handler))
+    loop.add_signal_handler(signal.SIGTERM, lambda: sigterm_handler())
 
     try:
         loop.run_until_complete(bot.start(TOKEN))
