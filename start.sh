bde27009cd56fbbdfdf6280a85546608086e4a73
Jackson Darrigo <31744342+jacksondarrigo@users.noreply.github.com>
2021-08-23 21:49:44 -0400

    Changed to yum to improve portability

--- start.sh
+++ start.sh
@@ -4,11 +4,9 @@ pip install --upgrade pip
 pip install --upgrade PyNaCl
 pip install --upgrade discord.py
 pip install --upgrade youtube-dl
-dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf install --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
-dnf install --nogpgcheck https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
-dnf config-manager --enable PowerTools
-dnf install ffmpeg
+yum install epel-release
+yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+yum install ffmpeg
 
 trap 'trap " " SIGTERM; kill 0; wait' SIGTERM
 
