c27792163b1ccbab051298f37c35ff5c6e85eded
Jackson Darrigo <31744342+jacksondarrigo@users.noreply.github.com>
2021-08-23 23:26:12 -0400

    Fixing my dumb mistake

--- start.sh
+++ start.sh
@@ -4,9 +4,7 @@ pip install --upgrade pip
 pip install --upgrade PyNaCl
 pip install --upgrade discord.py
 pip install --upgrade youtube-dl
-yum install epel-release
-yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
-yum install ffmpeg
+apt-get update && apt-get install -y ffmpeg
 
 trap 'trap " " SIGTERM; kill 0; wait' SIGTERM
 
